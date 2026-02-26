19b702d3b779198c6651565d64ed344338f2c060
Yassine Sayari <yassino.sayari@gmail.com>
2026-02-26 20:18:27 +0100

    eureka client setup

--- src/main/java/tn/esprit/reviewreportservice/ReviewReportServiceApplication.java
+++ src/main/java/tn/esprit/reviewreportservice/ReviewReportServiceApplication.java
@@ -2,8 +2,10 @@ package tn.esprit.reviewreportservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReviewReportServiceApplication {
 
 	public static void main(String[] args) {
